cc9a4e41fa53f96cdffab3618e3a81498bf26c4a
Kjartan Thor Wikfeldt <ktwikfeldt@gmail.com>
2017-02-17 09:18:46 +0100

    add debug example

---
+++ debug_example.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_log():
+    fig, ax = plt.subplots(2, 1)
+    x = np.linspace(1, 2, 10)
+    ax.plot(x, np.log(x))
+    plt.show()
+
+plot_log()  # Call the function, generate plot
